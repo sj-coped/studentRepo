fe0f172afc9af38b0a3cc0c2abfb07f4916426bf
sj-coped <jedidiahsuleh@gmail.com>
2025-01-22 12:05:22 +0000

    edited Lab01_Java in GitPod

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -5,6 +5,7 @@ void testMethod1()
   //First command x = y + z
   //Second command print(x)
   // Edit made by Jedidiah Suleh_240176367
+  // Edit made by Jedidiah in GitPod
 
 
   //Last line print('Ello')
